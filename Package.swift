d7aa7a26c36e27b877a5c94f16d22b1aa01d770b
Radu Ursache <radu@ursache.ro>
2021-08-27 11:34:37 +0300

    - updated package.swift

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
    targets: [
        .binaryTarget(
            name: "RSEssentials",
+           dependencies: ["PKHUD", "DeviceKit"],
            url: "https://github.com/rursache/RSEssentialsXC/raw/master/RSEssentials.xcframework.zip",
            checksum: "9dede4fc4dd075bc49f93e8d99b980c53c9440fc2b1d286c126f26826aab798f")
    ],
