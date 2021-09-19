c18584495c72463da5af2b74a52d8991d6e5eabb
Radu Ursache <radu@ursache.ro>
2021-09-19 16:23:26 +0300

    - updated framework

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5
 import PackageDescription
  
 let package = Package(
@@ -8,29 +7,38 @@ let package = Package(
    products: [
        .library(
            name: "RSEssentials",
-           targets: ["RSEssentialsTarget"]),
+           targets: ["RSEssentialsTarget"])
    ],
    dependencies: [
         .package(name: "PKHUD", url: "https://github.com/pkluz/PKHUD", .upToNextMajor(from: "5.4.0")),
-        .package(name: "DeviceKit", url: "https://github.com/devicekit/DeviceKit", .upToNextMajor(from: "4.5.0")),
+        .package(name: "DeviceKit", url: "https://github.com/devicekit/DeviceKit", .upToNextMajor(from: "4.5.0"))
    ],
    targets: [
        .target(
             name: "RSEssentialsTarget",
-            dependencies: [.target(name: "RSEssentialsWrapper")],
-            path: "RSEssentialsWrap"),
+            dependencies: [
+                .target(name: "RSEssentialsWrapper", condition: .when(platforms: [.iOS, .macCatalyst]))
+            ],
+            path: "RSEssentialsWrap")
        .target(
             name: "RSEssentialsWrapper",
-            dependencies: ["RSEssentials", "RSEssentialsDesignables"],
-            path: "RSEssentialsWrapper"),
+            dependencies: [
+                .byName(name: "PKHUD", condition: .when(platforms: [.iOS, .macCatalyst])),
+                .byName(name: "DeviceKit", condition: .when(platforms: [.iOS, .macCatalyst]))
+            ],
+            path: "RSEssentialsWrapper")
 		.target(
 			name: "RSEssentialsDesignables",
-			dependencies: ["RSEssentials", "PKHUD", "DeviceKit"],
-			path: "Files"),
+			dependencies: [
+                .byName(name: "RSEssentials", condition: .when(platforms: [.iOS, .macCatalyst])),
+                .byName(name: "PKHUD", condition: .when(platforms: [.iOS, .macCatalyst])),
+                .byName(name: "DeviceKit", condition: .when(platforms: [.iOS, .macCatalyst]))
+            ],
+			path: "Files")
        .binaryTarget(
            name: "RSEssentials",
            url: "https://github.com/rursache/RSEssentialsXC/raw/master/RSEssentials.xcframework.zip",
-           checksum: "9db88af7985be897718e76dc1d688c66aea0ef13d247bdff593a5c261dec67ba")
+           checksum: "6a790980e0653f88e9a0b495755812024275a3e2675c2a4bf2b16d8e7e149c1f")
    ],
    swiftLanguageVersions: [.v5]
 )
