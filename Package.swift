8179c146f79fced6b3119ce5a1c7151736c2e6e9
Radu Ursache <radu@ursache.ro>
2022-03-09 12:46:30 +0200

    - updated to v1.4.1

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
        .binaryTarget(
            name: "RSEssentials",
            url: "https://github.com/rursache/RSEssentialsXC/raw/master/RSEssentials.xcframework.zip",
-           checksum: "90ae1e48e2878abcb3ba25caf4c4a1b2002d9535c94722dc9369847cc04ebff4")
+           checksum: "9d7e991888146644492a288904cbdc5982b2d5c35b10a63c28b1eeb5d38b41d1")
    ],
    swiftLanguageVersions: [.v5]
 )
