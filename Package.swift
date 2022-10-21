4b9d8a721d1587f887b08cafac1e2c0ee94e6257
Jonathan Sanford <jonathan@obodo.app>
2022-10-21 16:40:54 -0700

    Rename bundle id

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
    platforms: [.iOS(.v13)],
    products: [
        .library(
-           name: "RSEssentials",
+           name: "ObodoRSEssentials",
            targets: ["RSEssentialsTarget"]),
    ],
    targets: [
